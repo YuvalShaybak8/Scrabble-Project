58bbea7bfd94e7132b4128f225fcb3a7e337e242
YuvalShaybak8 <yuvalsaybak@cs.colman.ac.il>
2022-11-16 17:59:50 +0200

    ..

--- Board.java
+++ Board.java
@@ -1,3 +1,38 @@
+//package test;
+
 public class Board {
-    
+    private static Tile boardArr[][] = null;
+    Word wordsArr[] = new Word[15];
+    int wordsCount = 0;
+
+    public Board getBoard() {
+        if (boardArr == null) {
+            boardArr = new Tile[15][15];
+        }
+        return this;
+    }
+
+    public Tile[][] getTiles() {
+        Tile copy[][] = boardArr.clone();
+        return copy;
+    }
+
+    public boolean boardLegal() {
+        if (boardArr[7][7] == null) {
+            return false;
+        }
+
+        for (Word w : wordsArr) {
+            if (w.getVertical()) {
+                if (w.getRow() + w.getTilesArr().length > 15) {
+                    return false;
+                }
+            } else {
+                if (w.getCol() + w.getTilesArr().length > 15) {
+                    return false;
+                }
+            }
+        }
+
+    }
 }
--- Word.java
+++ Word.java
@@ -1,3 +1,43 @@
+//package test;
+
 public class Word {
-    
+    private Tile tilesArr[] = new Tile[15];
+    private int col;
+    private int row;
+    private boolean vertical;
+
+    public Word(Tile[] tilesArr, int col, int row, boolean vertical) {
+        this.tilesArr = tilesArr;
+        this.col = col;
+        this.row = row;
+        this.vertical = vertical;
+    }
+
+    public boolean equals(Word other) {
+        if (this.col != other.col || this.row != other.row || this.vertical != other.vertical) {
+            return false;
+        }
+        for (int i = 0; i < 15; i++) {
+            if (this.tilesArr[i] != other.tilesArr[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public Tile[] getTilesArr() {
+        return tilesArr;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public boolean getVertical() {
+        return vertical;
+    }
 }
