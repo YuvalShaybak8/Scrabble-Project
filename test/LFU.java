f4d545d2fbb8e2bf1e04bc20307557582a833d5d
YuvalShaybak8 <yuvalsaybak@cs.colman.ac.il>
2022-12-11 03:15:30 +0200

    ..

--- test/LFU.java
+++ test/LFU.java
@@ -1,55 +1,33 @@
 package test;
 
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 
 public class LFU implements CacheReplacementPolicy{
+    private LinkedHashMap<String, Integer> map;
 
-    private String[] cache;
-    private int[] freq;
-    private int size;
-    private int count;
-    
-    public LFU(int size){
-        this.size = size;
-        this.cache = new String[size];
-        this.freq = new int[size];
-        this.count = 0;
+    public LFU() {
+        this.map = new LinkedHashMap<String, Integer>();
     }
-    
-    public void add(String word){
-        if(count < size){
-            cache[count] = word;
-            freq[count] = 1;
-            count++;
-        }else{
-            int min = freq[0];
-            int index = 0;
-            for(int i = 0; i < size; i++){
-                if(freq[i] < min){
-                    min = freq[i];
-                    index = i;
-                }
+
+    public void add(String word) 
+    {
+        if(map.containsKey(word)){
+            map.put(word, map.get(word) + 1);
+        } else {
+            map.put(word, 1);
+        }
+    }
+
+    public String remove() {
+        int min = Integer.MAX_VALUE;
+        String minWord = "";
+        for (String word : map.keySet()) {
+            if (map.get(word) <= min) {
+                min = map.get(word);
+                minWord = word;
             }
-            cache[index] = word;
-            freq[index] = 1;
         }
+        return minWord;
     }
-    
-    public String remove(){
-        String word = cache[0];
-        for(int i = 0; i < size - 1; i++){
-            cache[i] = cache[i + 1];
-            freq[i] = freq[i + 1];
-        }
-        count--;
-        return word;
-    }
-    
-    public String toString(){
-        String s = "";
-        for(int i = 0; i < count; i++){
-            s += cache[i] + " ";
-        }
-        return s;
-    }
-
 }
--- test/LRU.java
+++ test/LRU.java
@@ -1,5 +1,24 @@
 package test;
 
+import java.util.*;
 
-public class LRU implements CacheReplacementPolicy{
+public class LRU implements CacheReplacementPolicy {
+
+    private LinkedList<String> list = new LinkedList<String>();
+
+    public void add(String word) {
+        int index;
+        index = list.indexOf(word);
+        if (index != -1) {
+            String existWord = list.remove(index);
+            list.addFirst(existWord);
+        } else {
+            list.addFirst(word);
+        }
+    }
+
+    public String remove() {
+        String last = list.getLast();
+        return last;
+    }
 }
